a3baeeaddf9e8f433d3d7dad6cb19829e7636e4a
morth <morth@q1cc.net>
2013-01-16 15:01:41 +0100

    Assembler-Funktionsdefinition und Stackframe

--- default.c
+++ default.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <sys/time.h>
 
-int _asm_task(int, int, int);
+int calc(float* data1, float* data2, float* resul1, float* result2, int length);
 
 int main(int argc, char **argv) {
 	struct timeval start, ende;
 	gettimeofday(&start,0);
-	if(1 || 0 && 1 ^ 0) {
-		printf("ergebnis = %d \n", _asm_task(1,2,3));
-	}
+	
+	//calc(...)
+
 	gettimeofday(&ende, 0);
 	printf("time needed: %d usec \n", (ende.tv_sec - start.tv_sec)*1000000+ende.tv_usec-start.tv_usec);
 	return 0;
+
+	//TODO Rahmenprogramm
 }
--- default_asm.s
+++ default_asm.s
@@ -1,58 +1,34 @@
-.globl _asm_transpMatrix
-## void asm_transp_matrix(float* matrix, float* result)
-## rX = matrix
-## rY = result
-_asm_transpMatrix :
+.globl _calc
+## void calc(float* data1, float* data2, float* result1, float* result2, int length)
+## r0 = data1 -> [r11,#-0x8]
+## r1 = data2 -> [r11,#-0xc]
+## r2 = result1 -> [r11,#-0x10]
+## r3 = result2 -> [r11,#-0x14]
+## stack length -> [r11,#-0x18]
+## e1*pi -> r9
+## e2*pi -> r10
+_calc :
 	## Setup
 	## PUSH registers and stackframe
-	##PUSH {r11}
-	##MOV r11, sp
-	# Create new stckfrme
-	##SUB sp, sp, #0x0c
-	# cpy prms
-	##STR r0, [r11, #-0x08]
-	##STR r1, [r11, #-0x0c]
-	# loooooooop init
-	MOV r4, #0 ;# Zählvariable i = r4
-	MOV r5, #0 ;# Zählvariable j = r5
+	PUSH {r4-r10,r11}
+	# Create new stackfrme
+	ADD r11,sp, #40
+	SUB sp, sp, #20
+	# copy params
+	STR r0, [r11,#-0x8]
+	STR r1, [r11,#-0xc]
+	STR r2, [r11,#-0x10]
+	STR r3, [r11,#-0x14]
+	LDR r0, [r11,#4]
+	STR r0, [r11,#-0x18]
 
-loop :
-	# ld
-	LSL r9, r4, #3
-	ADD r6, r5, r9				;# 8*i+j + r0 := quellspeicher
-	LSL r6, r6, #2
-	ADD r6, r6, r0
-
-	LSL r9, r5, #3
-	ADD r7, r4, r9				;# 8*j+i + r1 := zielspeicher
-	LSL r7, r7, #2
-	ADD r7, r7, r1
-
-	# sve
-	LDR r8, [r6]
-	STR r8, [r7]
-
-	# lp
-	ADD r5, r5, #1
-	CMP r5, #8
-	BLT loop
-	ADD r4, r4, #1
-	CMP r4, #8
-	MOV r5, #0
-	BLT loop
-
-	## Trdwn
+	##Code:
+	##TODO
+	## Teardown
 
 	## Reset stackframe
-	# POP registers and stackfrm
+	SUB sp, r11, #40
+	# POP registers and stackframe
+	POP {r4-r10,r11}
 	# Return
 	BX lr
-
-
-.globl _asm_task
-## int asm_task(int, int, int)
-_asm_task :
-	ADD r0, r0, r1
-	ADD r0, r0, r2
-	BX lr
-
