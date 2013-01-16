8eb0589d3d0680b1e632c218032808e97d73d9ab
Claus Strasburger <claus@strasburger.de>
2013-01-16 17:35:59 +0100

    _capacity implementiert, funktioniert auch.

--- aspasmpi.c
+++ aspasmpi.c
@@ -6,7 +6,10 @@
 #define E_GUMMI		3.0
 #define E_PAPIER	5.0
 
-void calc(float* data1, float* data2, float* result1, float* result2, int length);
+extern void _calc(float* data1, float* data2, float* result1, float* result2, int length);
+extern float _capacity(float rad1, float rad2, float er);
+
+float calc_single_c (float r1, float r2, float e_r);
 void calc_c(float* data1, float* data2, float* result1, float* result2, int length);
 
 int main(int argc, char **argv) {
@@ -14,6 +17,11 @@ int main(int argc, char **argv) {
 	gettimeofday(&start,0);
 	
 	//calc(...)
+	float r1 = 0.9f, r2 = 1.0f, e = E_GUMMI;
+	float res = _capacity(r1, r2, e);
+	float res_c = calc_single_c(r1, r2, e);
+	printf("ergebnis: %e\n", res);
+	printf("ergebnis c: %e\n", res_c);
 
 	gettimeofday(&ende, 0);
 	printf("time needed: %d usec \n", (ende.tv_sec - start.tv_sec)*1000000+ende.tv_usec-start.tv_usec);
@@ -34,4 +42,12 @@ void calc_c (float* data1, float* data2, float* result1, float* result2, int len
 		result1[i] = k1;
 		result2[i] = k2;
 	}
+}
+
+float calc_single_c (float r1, float r2, float e_r) {
+	float faktor, bruch, k1;
+	faktor = 4 * M_PI * E_0;
+	bruch = (r2 * r1) / (r2 - r1);
+	k1 = faktor * bruch * e_r;
+	return k1;
 }
--- aspasmpi_asm.s
+++ aspasmpi_asm.s
@@ -12,7 +12,7 @@ _calc :
 	## PUSH registers and stackframe
 	PUSH {r4-r10,r11}
 	# Create new stackfrme
-	ADD r11,sp, #40
+	ADD r11,sp, #0x24
 	SUB sp, sp, #20
 	# copy params
 	STR r0, [r11,#-0x8]
@@ -27,8 +27,94 @@ _calc :
 	## Teardown
 
 	## Reset stackframe
-	SUB sp, r11, #40
+	SUB sp, r11, #0x24
 	# POP registers and stackframe
 	POP {r4-r10,r11}
 	# Return
 	BX lr
+
+
+.globl _capacity
+## float capacity(float rad1, float rad2, float er)
+## r0 = rad1 -> [r11,#-0x8]
+## r1 = rad2 -> [r11,#-0xc]
+## r2 = e_r -> [r11,#-0x10]
+_capacity :
+	## Setup
+	## PUSH registers and stackframe
+	PUSH {r5-r7,r11}
+	# Create new stackfrme
+	ADD r11,sp, #0xc
+	##Code:
+	## Platz in den VFP-Registern
+	FMRS r5, s3
+	FMRS r6, s4
+	FMRS r7, s5
+	PUSH {r5-r7}
+
+	## Faktor ausrechnen
+	FLDS s3, _M_PI
+	FLDS s4, _E_0
+
+	## s3 := pi*e0
+	FMULS s3, s3, s4
+
+	## float 4.0
+	FLDS s4, _M_4
+
+	## s3 := 4*pi*e0
+	FMULS s3, s3, s4
+
+	## e_r
+	FCPYS s4, s2
+
+	## s3 := 4*pi*e0*er
+	FMULS s3, s3, s4
+
+	## Bruch ausrechnen
+
+	## rad1
+	FCPYS s4, s0
+	## rad2
+	FCPYS s5, s1
+
+	## s4 := rad2-rad1
+	FSUBS s4, s5, s4
+
+	## s5 := rad1
+	FCPYS s5, s0
+
+	## s4 := rad1/(rad2-rad1)
+	FDIVS s4, s5, s4
+
+	## s5 := rad2
+	FCPYS s5, s1
+
+	## s4 := rad1*rad2 / (rad2-rad1)
+	FMULS s4, s4, s5
+
+	## s3 := kapazität
+	FMULS s3, s3, s4
+
+	## ergebnis schreiben
+	FCPYS s0, s3
+
+	## VFP-Register wiederherstellen
+	POP {r5-r7}
+	FMSR s3, r5
+	FMSR s4, r6
+	FMSR s5, r7
+	## Teardown
+	## Reset stackframe
+	SUB sp, r11, #0xc
+	# POP registers and stackframe
+	POP {r5-r7,r11}
+	# Return
+	BX lr
+
+_E_0 :
+	.float 8.85418781762e-12
+_M_PI :
+	.float 3.14159265358979323846
+_M_4 :
+	.float 4.0
